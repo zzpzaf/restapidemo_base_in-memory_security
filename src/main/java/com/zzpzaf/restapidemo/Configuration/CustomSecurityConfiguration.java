324baa32a336ead46342078a7020ca290d64258a
Panos Zafeiropoulos <zzpzaf@gmail.com>
2022-05-24 12:52:03 +0300

    tiny adjustments-refactoring

--- src/main/java/com/zzpzaf/restapidemo/Configuration/CustomSecurityConfiguration.java
+++ src/main/java/com/zzpzaf/restapidemo/Configuration/CustomSecurityConfiguration.java
@@ -1,4 +1,4 @@
-package com.Configuration;
+package com.zzpzaf.restapidemo.Configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -6,17 +6,22 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+
+
 @Configuration
 @EnableWebSecurity
 public class CustomSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+
        @Override
        protected void configure(HttpSecurity http) throws Exception {
-               http.authorizeRequests().antMatchers("/**").hasRole("USER").and().httpBasic();
+               http.authorizeRequests().antMatchers("/api/items").hasRole("USER").and().httpBasic();
        }
 
        @Override
        protected void configure(AuthenticationManagerBuilder auth) throws Exception {                
-               auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+               auth.inMemoryAuthentication()
+               .withUser("user").password("{noop}mypassword").roles("USER");
        }
 }
+
